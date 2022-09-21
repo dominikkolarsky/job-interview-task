f12a366317e6caed3929eae708a8175c87c0e2fc
Dominik Kolarsky <kolarskydominik@gmail.com>
2022-09-21 12:24:09 +0200

    done: navbar, header, copyright

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+// ----- meters counter logic
+const element = document.getElementById("counter__number");
+
+window.onload = () => {
+    setInterval(count, 100);
+};
+
+let counter = 0;
+function count() {
+    counter++;
+    //console.log(counter)
+    element.innerHTML = counter;
+}
+
+// ----- NAV COLOR ON SCROLL 
+const navbar = document.getElementById("navbar")
+window.addEventListener("scroll", changeNavBg);
+
+function changeNavBg() {
+    if (window.scrollY >= 80) {
+        navbar.classList.add("active");
+    } else {
+        navbar.classList.remove("active");
+    }
+};
+
+// ----- MOBILE MENU
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#navMenu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", openMenu);
+
+function openMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navbar.classList.add("active");
+}
+
+//closing logic
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    header.classList.remove("active");
+}
